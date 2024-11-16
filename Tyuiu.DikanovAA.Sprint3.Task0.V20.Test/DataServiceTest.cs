a0f208d88079604dbf2b7c71d18c18e2799b6f1f
Tyuiu.DikanovAA@outlook.com <Tyuiu.DikanovAA@outlook.com>
2024-11-16 11:59:31 +0500

    Add project files.

---
+++ Tyuiu.DikanovAA.Sprint3.Task0.V20.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+using Tyuiu.DikanovAA.Sprint3.Task0.V20.Lib;
+namespace Tyuiu.DikanovAA.Sprint3.Task0.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int value = 0;
+            int startValue = 1;
+            int stopValue = 14;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+
+            double wait = 4;
+
+            Assert.AreEqual(wait, res);
+
+
+        }
+    }
+}
---
+++ Tyuiu.DikanovAA.Sprint3.Task0.V20/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.DikanovAA.Sprint3.Task0.V20.Lib;
+namespace Tyuiu.DikanovAA.Sprint3.Task0.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 14;
+
+            Console.WriteLine(ds.GetMultiplySeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
