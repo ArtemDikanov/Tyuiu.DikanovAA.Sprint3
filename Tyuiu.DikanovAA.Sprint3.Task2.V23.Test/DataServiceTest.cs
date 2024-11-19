c281d6d4397e6423c48c08425c39259cd920834f
Tyuiu.DikanovAA@outlook.com <Tyuiu.DikanovAA@outlook.com>
2024-11-19 13:37:18 +0500

    Task2

---
+++ Tyuiu.DikanovAA.Sprint3.Task2.V23.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+using Tyuiu.DikanovAA.Sprint3.Task2.V23.Lib;
+namespace Tyuiu.DikanovAA.Sprint3.Task2.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            double value = 1.5;
+            int startValue = 1;
+            int stopValue = 13;
+
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+
+            double wait = -63.8;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DikanovAA.Sprint3.Task2.V23/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.DikanovAA.Sprint3.Task2.V23.Lib;
+namespace Tyuiu.DikanovAA.Sprint3.Task2.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            double value = Convert.ToDouble(Console.ReadLine());
+            int startValue = 1;
+            int stopValue = 13;
+
+            Console.WriteLine(ds.GetSumSeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
