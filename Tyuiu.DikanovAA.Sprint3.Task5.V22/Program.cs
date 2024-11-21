bbba392a5b279c1000deb1490f6ff68235843037
Tyuiu.DikanovAA@outlook.com <Tyuiu.DikanovAA@outlook.com>
2024-11-21 14:24:20 +0500

    Task5

---
+++ Tyuiu.DikanovAA.Sprint3.Task5.V22/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.DikanovAA.Sprint3.Task5.V22.Lib;
+namespace Tyuiu.DikanovAA.Sprint3.Task5.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x = 2;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 12;
+
+           
+            Console.WriteLine("Ответ: " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+            
+
+        }
+    }
+}
